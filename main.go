2b9a45c3d5ace267cce1ffea50ac50a4ccdcf9d9
ThitipongPP <thitipongpp@MacBook-Air-khxng-ThitipongPP.local>
2026-02-25 00:14:13 +0700

    Auto send request to url from urlList

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func main() {
+	urlLists := []string{"https://google.com", "https://this-web-does-not-exist.com", "https://github.com"}
+
+	for _, url := range urlLists {
+		res, err := http.Get(url)
+
+		if err != nil {
+			fmt.Println("Cannot get response from ", url)
+			fmt.Println("Error :", err.Error())
+			continue
+		}
+
+		fmt.Println("URL:", url)
+		fmt.Println("Response code:", res.StatusCode)
+		res.Body.Close()
+	}
 }
